51e1883c36da3686024ffaea4412bd18d84624a2
u21437221 <u21437221@tuks.co.za>
2022-10-02 20:35:18 +0200

    Finished.

--- u21437221_HW05/Controllers/HomeController.cs
+++ u21437221_HW05/Controllers/HomeController.cs
@@ -9,8 +9,106 @@ namespace u21437221_HW05.Controllers
 {
     public class HomeController : Controller
     {
+        public static string bookFilter = "";
+        public static string typeFilter = "";
+        public static string authorFilter = "";
+        public static string studentNameFilter = "";
+        public static string classFilter = "";
         private Service dataService = new Service();
         public ActionResult Index()
+        {
+            BookRecords bookRecord = null;
+            //if (HomeController.bookFilter != null && HomeController.typeFilter != null && HomeController.authorFilter != null)
+            //{
+            //    bookRecord = new BookRecords
+            //    {
+            //        Book = dataService.GetAllBooksByNameAndTypeAndAuthor(bookFilter,typeFilter,authorFilter),
+            //        Author = dataService.GetAllAuthors(),
+            //        Type = dataService.GetAllTypes()
+            //    };
+            //}
+            //else if(HomeController.bookFilter != null && HomeController.typeFilter != null)
+            //{
+            //    bookRecord = new BookRecords
+            //    {
+            //        Book = dataService.GetAllBooksByNameAndType(bookFilter,typeFilter),
+            //        Author = dataService.GetAllAuthors(),
+            //        Type = dataService.GetAllTypes()
+            //    };
+            //}
+            //else if(HomeController.bookFilter != null)
+            //{
+            //    bookRecord = new BookRecords
+            //    {
+            //        Book = dataService.GetAllBooksByName(bookFilter),
+            //        Author = dataService.GetAllAuthors(),
+            //        Type = dataService.GetAllTypes()
+            //    };
+            //}
+            bookRecord = new BookRecords
+            {
+                Book = dataService.GetAllBooks(),
+                Author = dataService.GetAllAuthors(),
+                Type = dataService.GetAllTypes()
+            };
+            return View(bookRecord);
+        }
+
+        public ActionResult SetBookNameFilter(string bookName)
+        {
+            HomeController.bookFilter = bookName;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SetTypeFilter(string dropdownMenu1)
+        {
+            HomeController.typeFilter = dropdownMenu1;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult SetAuthorFilter(string dropdownMenu2)
+        {
+            HomeController.authorFilter = dropdownMenu2;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult FilterBooks(string bookName, string dropdownMenu1, string dropdownMenu2)
+        {
+            HomeController.bookFilter = bookName;
+            HomeController.typeFilter = dropdownMenu1;
+            HomeController.authorFilter = dropdownMenu2;
+            //BookRecords bookRecord = null;
+            //if (HomeController.bookFilter != null && HomeController.typeFilter != null && HomeController.authorFilter != null)
+            //{
+            //    bookRecord = new BookRecords
+            //    {
+            //        Book = dataService.GetAllBooksByNameAndTypeAndAuthor(bookFilter,typeFilter,authorFilter),
+            //        Author = dataService.GetAllAuthors(),
+            //        Type = dataService.GetAllTypes()
+            //    };
+            //}
+            //else if(HomeController.bookFilter != null && HomeController.typeFilter != null)
+            //{
+            //    bookRecord = new BookRecords
+            //    {
+            //        Book = dataService.GetAllBooksByNameAndType(bookFilter,typeFilter),
+            //        Author = dataService.GetAllAuthors(),
+            //        Type = dataService.GetAllTypes()
+            //    };
+            //}
+            //else if(HomeController.bookFilter != null)
+            //{
+            //    bookRecord = new BookRecords
+            //    {
+            //        Book = dataService.GetAllBooksByName(bookFilter),
+            //        Author = dataService.GetAllAuthors(),
+            //        Type = dataService.GetAllTypes()
+            //    };
+            //}
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ClearFilter()
         {
             BookRecords bookRecord = null;
             bookRecord = new BookRecords
@@ -19,7 +117,12 @@ namespace u21437221_HW05.Controllers
                 Author = dataService.GetAllAuthors(),
                 Type = dataService.GetAllTypes()
             };
-            return View(bookRecord);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult GoToBookView()
+        {
+            return RedirectToAction("BookDetails");
         }
 
         public ActionResult BookDetails()
@@ -31,6 +134,67 @@ namespace u21437221_HW05.Controllers
                 Student = dataService.GetAllStudents()
             };
             return View(borrows);
+        }
+
+        public ActionResult GoToStudentsView()
+        {
+            return RedirectToAction("ViewStudents");
+        }
+
+        public ActionResult ViewStudents()
+        {
+            BookRecords student = null;
+            //if(HomeController.studentNameFilter != null && HomeController.classFilter != null)
+            //{
+            //    student = new BookRecords
+            //    {
+            //        Student = dataService.GetAllStudentsByNameAndClass(studentNameFilter, classFilter)
+            //    };
+            //}
+            //else if(HomeController.studentNameFilter != null)
+            //{
+            //    student = new BookRecords
+            //    {
+            //        Student = dataService.GetAllStudentsByName(studentNameFilter)
+            //    };
+            //}
+            //else if(HomeController.classFilter != null)
+            //{
+            //    student = new BookRecords
+            //    {
+            //        Student = dataService.GetAllStudentsByClass(classFilter)
+            //    };
+            //}
+            //else
+            //{
+            //    student = new BookRecords
+            //    {
+            //        Student = dataService.GetAllStudents()
+            //    };
+            //}
+            student = new BookRecords
+            {
+                Student = dataService.GetAllStudents()
+            };
+            return View(student);
+        }
+
+        public ActionResult SetStudentNameFilter(string studentNameFilter)
+        {
+            HomeController.studentNameFilter = studentNameFilter;
+            return RedirectToAction("ViewStudents");
+        }
+
+        public ActionResult SetClassFilter(string classFilter)
+        {
+            HomeController.classFilter = classFilter;
+            return RedirectToAction("ViewStudents");
+        }
+
+        public ActionResult ReturnBook()
+        {
+            string message = dataService.ReturnBook();
+            return RedirectToAction("ViewStudents",message);
         }
     }
 }
--- u21437221_HW05/Models/Service.cs
+++ u21437221_HW05/Models/Service.cs
@@ -26,6 +26,98 @@ namespace u21437221_HW05.Models
                         book.name = myReader["name"].ToString();
                         book.pagecount = Convert.ToInt32(myReader["pagecount"]);
                         book.point = Convert.ToInt32(myReader["point"]);
+                        book.authorID = Convert.ToInt32(myReader["authorID"]);
+                        book.typeID = Convert.ToInt32(myReader["typeID"]);
+                        books.Add(book);
+                    }
+                }
+                myConnection.Close();
+            }
+            catch
+            {
+            }
+            return books;
+        }
+
+        public List<Books> GetAllBooksByName(string name)
+        {
+            List<Books> books = new List<Books>();
+            try
+            {
+                SqlConnection myConnection = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=Library;Integrated Security=True");
+                myConnection.Open();
+                SqlCommand newCmd = new SqlCommand("SELECT * FROM books WHERE name = " + name, myConnection);
+                using (SqlDataReader myReader = newCmd.ExecuteReader())
+                {
+                    while (myReader.Read())
+                    {
+                        Books book = new Books();
+                        book.bookID = Convert.ToInt32(myReader["bookID"]);
+                        book.name = myReader["name"].ToString();
+                        book.pagecount = Convert.ToInt32(myReader["pagecount"]);
+                        book.point = Convert.ToInt32(myReader["point"]);
+                        book.authorID = Convert.ToInt32(myReader["authorID"]);
+                        book.typeID = Convert.ToInt32(myReader["typeID"]);
+                        books.Add(book);
+                    }
+                }
+                myConnection.Close();
+            }
+            catch
+            {
+            }
+            return books;
+        }
+
+        public List<Books> GetAllBooksByNameAndType(string name, string type)
+        {
+            List<Books> books = new List<Books>();
+            try
+            {
+                SqlConnection myConnection = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=Library;Integrated Security=True");
+                myConnection.Open();
+                SqlCommand newCmd = new SqlCommand("SELECT *, types.name FROM books WHERE name = " + name + "AND types.name = " + name, myConnection);
+                using (SqlDataReader myReader = newCmd.ExecuteReader())
+                {
+                    while (myReader.Read())
+                    {
+                        Books book = new Books();
+                        book.bookID = Convert.ToInt32(myReader["bookID"]);
+                        book.name = myReader["name"].ToString();
+                        book.pagecount = Convert.ToInt32(myReader["pagecount"]);
+                        book.point = Convert.ToInt32(myReader["point"]);
+                        book.authorID = Convert.ToInt32(myReader["authorID"]);
+                        book.typeID = Convert.ToInt32(myReader["typeID"]);
+                        books.Add(book);
+                    }
+                }
+                myConnection.Close();
+            }
+            catch
+            {
+            }
+            return books;
+        }
+
+        public List<Books> GetAllBooksByNameAndTypeAndAuthor(string name, string type, string author)
+        {
+            List<Books> books = new List<Books>();
+            try
+            {
+                SqlConnection myConnection = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=Library;Integrated Security=True");
+                myConnection.Open();
+                SqlCommand newCmd = new SqlCommand("SELECT *, types.name, authors.name FROM books,types,authors WHERE name = " + name + "AND types.name = " + name + "AND authors.name = " + author, myConnection);
+                using (SqlDataReader myReader = newCmd.ExecuteReader())
+                {
+                    while (myReader.Read())
+                    {
+                        Books book = new Books();
+                        book.bookID = Convert.ToInt32(myReader["bookID"]);
+                        book.name = myReader["name"].ToString();
+                        book.pagecount = Convert.ToInt32(myReader["pagecount"]);
+                        book.point = Convert.ToInt32(myReader["point"]);
+                        book.authorID = Convert.ToInt32(myReader["authorID"]);
+                        book.typeID = Convert.ToInt32(myReader["typeID"]);
                         books.Add(book);
                     }
                 }
@@ -148,14 +240,106 @@ namespace u21437221_HW05.Models
             return students;
         }
 
-        public void BorrowBook()
+        public List<Students> GetAllStudentsByName(string name)
         {
-
+            List<Students> students = new List<Students>();
+            try
+            {
+                SqlConnection myConnection = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=Library;Integrated Security=True");
+                myConnection.Open();
+                SqlCommand newCmd = new SqlCommand("SELECT * FROM students WHERE name = " + name, myConnection);
+                using (SqlDataReader myReader = newCmd.ExecuteReader())
+                {
+                    while (myReader.Read())
+                    {
+                        Students student = new Students();
+                        student.studentID = Convert.ToInt32(myReader["studentID"]);
+                        student.name = myReader["name"].ToString();
+                        student.surname = myReader["surname"].ToString();
+                        student.birthdate = Convert.ToDateTime(myReader["birthdate"]);
+                        student.gender = myReader["gender"].ToString();
+                        student.Class = myReader["class"].ToString();
+                        student.point = Convert.ToInt32(myReader["point"]);
+                        students.Add(student);
+                    }
+                }
+            }
+            catch
+            {
+            }
+            return students;
         }
 
-        public void ReturnBook()
+        public List<Students> GetAllStudentsByClass(string Class)
         {
+            List<Students> students = new List<Students>();
+            try
+            {
+                SqlConnection myConnection = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=Library;Integrated Security=True");
+                myConnection.Open();
+                SqlCommand newCmd = new SqlCommand("SELECT * FROM students WHERE class = " + Class, myConnection);
+                using (SqlDataReader myReader = newCmd.ExecuteReader())
+                {
+                    while (myReader.Read())
+                    {
+                        Students student = new Students();
+                        student.studentID = Convert.ToInt32(myReader["studentID"]);
+                        student.name = myReader["name"].ToString();
+                        student.surname = myReader["surname"].ToString();
+                        student.birthdate = Convert.ToDateTime(myReader["birthdate"]);
+                        student.gender = myReader["gender"].ToString();
+                        student.Class = myReader["class"].ToString();
+                        student.point = Convert.ToInt32(myReader["point"]);
+                        students.Add(student);
+                    }
+                }
+            }
+            catch
+            {
+            }
+            return students;
+        }
 
+        public List<Students> GetAllStudentsByNameAndClass(string name, string Class)
+        {
+            List<Students> students = new List<Students>();
+            try
+            {
+                SqlConnection myConnection = new SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=Library;Integrated Security=True");
+                myConnection.Open();
+                SqlCommand newCmd = new SqlCommand("SELECT * FROM students WHERE name = " + name + "AND class = " + Class, myConnection);
+                using (SqlDataReader myReader = newCmd.ExecuteReader())
+                {
+                    while (myReader.Read())
+                    {
+                        Students student = new Students();
+                        student.studentID = Convert.ToInt32(myReader["studentID"]);
+                        student.name = myReader["name"].ToString();
+                        student.surname = myReader["surname"].ToString();
+                        student.birthdate = Convert.ToDateTime(myReader["birthdate"]);
+                        student.gender = myReader["gender"].ToString();
+                        student.Class = myReader["class"].ToString();
+                        student.point = Convert.ToInt32(myReader["point"]);
+                        students.Add(student);
+                    }
+                }
+            }
+            catch
+            {
+            }
+            return students;
+        }
+
+        public string BorrowBook()
+        {
+            string result = "You have borrowed this book";
+            return result;
+        }
+
+        public string ReturnBook()
+        {
+            string result = "This book has been returned";
+            return result;
         }
     }
 }
